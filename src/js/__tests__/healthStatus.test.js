db224b7a464dd72c181ff41efb5e394fd7e18146
laps78 <9254384@mail.ru>
2022-09-07 14:56:31 +0300

    initial commit, the task is solved

---
+++ src/js/__tests__/healthStatus.test.js
@@ -0,0 +1,13 @@
+import healthStatusBar from '../healthStatusBar';
+
+test.each([
+  [100,'healthy'],
+  [51, 'healthy'],
+  [50, 'wounded'],
+  [21, 'wounded'],
+  [15, 'wounded'],
+  [14, 'critical'],
+])('health status bar should return right values', (healthStatus, expected) => {
+  const result = healthStatusBar(healthStatus);
+  expect(result).toBe(expected);
+})
